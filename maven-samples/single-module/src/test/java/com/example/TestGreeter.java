2ce8978cff5aa99fcab31382f895bf562fdd97c6
missshifuu <68296871+missshifuu@users.noreply.github.com>
2024-05-14 06:56:29 +0530

    Updated TestGreeter.java
    Added a intentional failed test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,4 +41,9 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+    @Test
+  public void intentionallyFailed() {
+    assertTrue(false);
+  }
 }
